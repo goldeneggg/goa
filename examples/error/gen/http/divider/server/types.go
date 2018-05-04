8cab1c886a8a15d7474773ab3e2a1489ad35d1f3
Nitin Mohan <nitin@rightscale.com>
2018-05-04 14:32:53 -0700

    Project result type with single view in server response
    * Server response types use pointers for all attributes
    * Server response is projected using default view unless specified

--- examples/error/gen/http/divider/server/types.go
+++ examples/error/gen/http/divider/server/types.go
@@ -17,91 +17,91 @@ import (
 // "integer_divide" endpoint HTTP response body for the "has_remainder" error.
 type IntegerDivideHasRemainderResponseBody struct {
 	// Name is the name of this class of errors.
-	Name string `form:"name" json:"name" xml:"name"`
+	Name *string `form:"name,omitempty" json:"name,omitempty" xml:"name,omitempty"`
 	// ID is a unique identifier for this particular occurrence of the problem.
-	ID string `form:"id" json:"id" xml:"id"`
+	ID *string `form:"id,omitempty" json:"id,omitempty" xml:"id,omitempty"`
 	// Message is a human-readable explanation specific to this occurrence of the
 	// problem.
-	Message string `form:"message" json:"message" xml:"message"`
+	Message *string `form:"message,omitempty" json:"message,omitempty" xml:"message,omitempty"`
 	// Is the error temporary?
-	Temporary bool `form:"temporary" json:"temporary" xml:"temporary"`
+	Temporary *bool `form:"temporary,omitempty" json:"temporary,omitempty" xml:"temporary,omitempty"`
 	// Is the error a timeout?
-	Timeout bool `form:"timeout" json:"timeout" xml:"timeout"`
+	Timeout *bool `form:"timeout,omitempty" json:"timeout,omitempty" xml:"timeout,omitempty"`
 }
 
 // IntegerDivideDivByZeroResponseBody is the type of the "divider" service
 // "integer_divide" endpoint HTTP response body for the "div_by_zero" error.
 type IntegerDivideDivByZeroResponseBody struct {
 	// Name is the name of this class of errors.
-	Name string `form:"name" json:"name" xml:"name"`
+	Name *string `form:"name,omitempty" json:"name,omitempty" xml:"name,omitempty"`
 	// ID is a unique identifier for this particular occurrence of the problem.
-	ID string `form:"id" json:"id" xml:"id"`
+	ID *string `form:"id,omitempty" json:"id,omitempty" xml:"id,omitempty"`
 	// Message is a human-readable explanation specific to this occurrence of the
 	// problem.
-	Message string `form:"message" json:"message" xml:"message"`
+	Message *string `form:"message,omitempty" json:"message,omitempty" xml:"message,omitempty"`
 	// Is the error temporary?
-	Temporary bool `form:"temporary" json:"temporary" xml:"temporary"`
+	Temporary *bool `form:"temporary,omitempty" json:"temporary,omitempty" xml:"temporary,omitempty"`
 	// Is the error a timeout?
-	Timeout bool `form:"timeout" json:"timeout" xml:"timeout"`
+	Timeout *bool `form:"timeout,omitempty" json:"timeout,omitempty" xml:"timeout,omitempty"`
 }
 
 // IntegerDivideTimeoutResponseBody is the type of the "divider" service
 // "integer_divide" endpoint HTTP response body for the "timeout" error.
 type IntegerDivideTimeoutResponseBody struct {
 	// Name is the name of this class of errors.
-	Name string `form:"name" json:"name" xml:"name"`
+	Name *string `form:"name,omitempty" json:"name,omitempty" xml:"name,omitempty"`
 	// ID is a unique identifier for this particular occurrence of the problem.
-	ID string `form:"id" json:"id" xml:"id"`
+	ID *string `form:"id,omitempty" json:"id,omitempty" xml:"id,omitempty"`
 	// Message is a human-readable explanation specific to this occurrence of the
 	// problem.
-	Message string `form:"message" json:"message" xml:"message"`
+	Message *string `form:"message,omitempty" json:"message,omitempty" xml:"message,omitempty"`
 	// Is the error temporary?
-	Temporary bool `form:"temporary" json:"temporary" xml:"temporary"`
+	Temporary *bool `form:"temporary,omitempty" json:"temporary,omitempty" xml:"temporary,omitempty"`
 	// Is the error a timeout?
-	Timeout bool `form:"timeout" json:"timeout" xml:"timeout"`
+	Timeout *bool `form:"timeout,omitempty" json:"timeout,omitempty" xml:"timeout,omitempty"`
 }
 
 // DivideDivByZeroResponseBody is the type of the "divider" service "divide"
 // endpoint HTTP response body for the "div_by_zero" error.
 type DivideDivByZeroResponseBody struct {
 	// Name is the name of this class of errors.
-	Name string `form:"name" json:"name" xml:"name"`
+	Name *string `form:"name,omitempty" json:"name,omitempty" xml:"name,omitempty"`
 	// ID is a unique identifier for this particular occurrence of the problem.
-	ID string `form:"id" json:"id" xml:"id"`
+	ID *string `form:"id,omitempty" json:"id,omitempty" xml:"id,omitempty"`
 	// Message is a human-readable explanation specific to this occurrence of the
 	// problem.
-	Message string `form:"message" json:"message" xml:"message"`
+	Message *string `form:"message,omitempty" json:"message,omitempty" xml:"message,omitempty"`
 	// Is the error temporary?
-	Temporary bool `form:"temporary" json:"temporary" xml:"temporary"`
+	Temporary *bool `form:"temporary,omitempty" json:"temporary,omitempty" xml:"temporary,omitempty"`
 	// Is the error a timeout?
-	Timeout bool `form:"timeout" json:"timeout" xml:"timeout"`
+	Timeout *bool `form:"timeout,omitempty" json:"timeout,omitempty" xml:"timeout,omitempty"`
 }
 
 // DivideTimeoutResponseBody is the type of the "divider" service "divide"
 // endpoint HTTP response body for the "timeout" error.
 type DivideTimeoutResponseBody struct {
 	// Name is the name of this class of errors.
-	Name string `form:"name" json:"name" xml:"name"`
+	Name *string `form:"name,omitempty" json:"name,omitempty" xml:"name,omitempty"`
 	// ID is a unique identifier for this particular occurrence of the problem.
-	ID string `form:"id" json:"id" xml:"id"`
+	ID *string `form:"id,omitempty" json:"id,omitempty" xml:"id,omitempty"`
 	// Message is a human-readable explanation specific to this occurrence of the
 	// problem.
-	Message string `form:"message" json:"message" xml:"message"`
+	Message *string `form:"message,omitempty" json:"message,omitempty" xml:"message,omitempty"`
 	// Is the error temporary?
-	Temporary bool `form:"temporary" json:"temporary" xml:"temporary"`
+	Temporary *bool `form:"temporary,omitempty" json:"temporary,omitempty" xml:"temporary,omitempty"`
 	// Is the error a timeout?
-	Timeout bool `form:"timeout" json:"timeout" xml:"timeout"`
+	Timeout *bool `form:"timeout,omitempty" json:"timeout,omitempty" xml:"timeout,omitempty"`
 }
 
 // NewIntegerDivideHasRemainderResponseBody builds the HTTP response body from
 // the result of the "integer_divide" endpoint of the "divider" service.
 func NewIntegerDivideHasRemainderResponseBody(res *goa.ServiceError) *IntegerDivideHasRemainderResponseBody {
 	body := &IntegerDivideHasRemainderResponseBody{
-		Name:      res.Name,
-		ID:        res.ID,
-		Message:   res.Message,
-		Temporary: res.Temporary,
-		Timeout:   res.Timeout,
+		Name:      &res.Name,
+		ID:        &res.ID,
+		Message:   &res.Message,
+		Temporary: &res.Temporary,
+		Timeout:   &res.Timeout,
 	}
 	return body
 }
@@ -110,11 +110,11 @@ func NewIntegerDivideHasRemainderResponseBody(res *goa.ServiceError) *IntegerDiv
 // result of the "integer_divide" endpoint of the "divider" service.
 func NewIntegerDivideDivByZeroResponseBody(res *goa.ServiceError) *IntegerDivideDivByZeroResponseBody {
 	body := &IntegerDivideDivByZeroResponseBody{
-		Name:      res.Name,
-		ID:        res.ID,
-		Message:   res.Message,
-		Temporary: res.Temporary,
-		Timeout:   res.Timeout,
+		Name:      &res.Name,
+		ID:        &res.ID,
+		Message:   &res.Message,
+		Temporary: &res.Temporary,
+		Timeout:   &res.Timeout,
 	}
 	return body
 }
@@ -123,11 +123,11 @@ func NewIntegerDivideDivByZeroResponseBody(res *goa.ServiceError) *IntegerDivide
 // result of the "integer_divide" endpoint of the "divider" service.
 func NewIntegerDivideTimeoutResponseBody(res *goa.ServiceError) *IntegerDivideTimeoutResponseBody {
 	body := &IntegerDivideTimeoutResponseBody{
-		Name:      res.Name,
-		ID:        res.ID,
-		Message:   res.Message,
-		Temporary: res.Temporary,
-		Timeout:   res.Timeout,
+		Name:      &res.Name,
+		ID:        &res.ID,
+		Message:   &res.Message,
+		Temporary: &res.Temporary,
+		Timeout:   &res.Timeout,
 	}
 	return body
 }
@@ -136,11 +136,11 @@ func NewIntegerDivideTimeoutResponseBody(res *goa.ServiceError) *IntegerDivideTi
 // of the "divide" endpoint of the "divider" service.
 func NewDivideDivByZeroResponseBody(res *goa.ServiceError) *DivideDivByZeroResponseBody {
 	body := &DivideDivByZeroResponseBody{
-		Name:      res.Name,
-		ID:        res.ID,
-		Message:   res.Message,
-		Temporary: res.Temporary,
-		Timeout:   res.Timeout,
+		Name:      &res.Name,
+		ID:        &res.ID,
+		Message:   &res.Message,
+		Temporary: &res.Temporary,
+		Timeout:   &res.Timeout,
 	}
 	return body
 }
@@ -149,11 +149,11 @@ func NewDivideDivByZeroResponseBody(res *goa.ServiceError) *DivideDivByZeroRespo
 // of the "divide" endpoint of the "divider" service.
 func NewDivideTimeoutResponseBody(res *goa.ServiceError) *DivideTimeoutResponseBody {
 	body := &DivideTimeoutResponseBody{
-		Name:      res.Name,
-		ID:        res.ID,
-		Message:   res.Message,
-		Temporary: res.Temporary,
-		Timeout:   res.Timeout,
+		Name:      &res.Name,
+		ID:        &res.ID,
+		Message:   &res.Message,
+		Temporary: &res.Temporary,
+		Timeout:   &res.Timeout,
 	}
 	return body
 }
